2cd9c791fb9e44a21302e2b0244249c7522a3373
futurewp <1154944904@qq.com>
2020-12-01 15:56:51 +0800

    去除空格，以防打包有问题

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -54,23 +54,21 @@ public class StatusBar extends CordovaPlugin {
         this.cordova.getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                // Clear flag FLAG_FORCE_NOT_FULLSCREEN which is set initially
-                // by the Cordova.
-                Window window = cordova.getActivity().getWindow();
-                //添加内容start
-                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                // window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                window.setStatusBarColor(Color.TRANSPARENT);
-                // window.setNavigationBarColor(Color.TRANSPARENT);
-                //添加内容end
-                window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
-                // Read 'StatusBarBackgroundColor' from config.xml, default is #000000.
-                // setStatusBarBackgroundColor(preferences.getString("StatusBarBackgroundColor", "#000000"));
-
-                // Read 'StatusBarStyle' from config.xml, default is 'lightcontent'.
-                // setStatusBarStyle(preferences.getString("StatusBarStyle", "lightcontent"));
+// Clear flag FLAG_FORCE_NOT_FULLSCREEN which is set initially
+// by the Cordova.
+Window window = cordova.getActivity().getWindow();
+//添加内容start
+window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+// window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+window.setStatusBarColor(Color.TRANSPARENT);
+// window.setNavigationBarColor(Color.TRANSPARENT);
+//添加内容end
+window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+// Read 'StatusBarBackgroundColor' from config.xml, default is #000000.
+// setStatusBarBackgroundColor(preferences.getString("StatusBarBackgroundColor", "#000000"));
+// Read 'StatusBarStyle' from config.xml, default is 'lightcontent'.
+// setStatusBarStyle(preferences.getString("StatusBarStyle", "lightcontent"));
             }
         });
     }
